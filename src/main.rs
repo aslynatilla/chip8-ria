9233b0d90c5ec586282cb3a7ff7dc4beb4bcb639
Antonio Natilla <antonio.natilla@studenti.unimi.it>
2021-07-12 17:07:54 +0200

    Call and return implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{u16, usize};
-
 struct CPU {
     registers: [u8; 16],
     program_counter: usize,
-    memory: [u8; 0x1000], // 0x1000 == 2^12 in binary => 4096 bytes, that is 4kb
+    memory: [u8; 0x1000],
+    stack: [u16; 16],
+    stack_pointer: usize,
 }
 
 impl CPU {
@@ -25,11 +25,16 @@ impl CPU {
             let y = ((op_code & 0x00F0) >> 4) as u8;
             let d = ((op_code & 0x000F) >> 0) as u8;
 
+            let nnn = op_code & 0x0FFF;
+
             match (c, x, y, d) {
                 (0, 0, 0, 0) => {
                     return;
                 }
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
+
                 _ => todo!("opcode {:04x}", op_code),
             }
         }
@@ -46,6 +51,27 @@ impl CPU {
             false => self.registers[0xF] = 0,
         }
     }
+
+    fn call(&mut self, fn_address: u16) {
+        let (stack_ptr, stack) = (self.stack_pointer, &mut self.stack);
+
+        if stack_ptr > stack.len() {
+            panic!("Stack overflow!");
+        }
+
+        stack[stack_ptr] = self.program_counter as u16;
+        self.stack_pointer += 1;
+        self.program_counter = fn_address as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow!");
+        }
+
+        self.stack_pointer -= 1;
+        self.program_counter = self.stack[self.stack_pointer] as usize;
+    }
 }
 
 fn main() {
@@ -53,21 +79,16 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         program_counter: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
-    cpu.registers[0] = 5;
-    cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
+    cpu.registers[0..2].copy_from_slice(&[5, 10]);
 
     let mem = &mut cpu.memory;
-    mem[0] = 0x80;
-    mem[1] = 0x14;
-    mem[2] = 0x80;
-    mem[3] = 0x24;
-    mem[4] = 0x80;
-    mem[5] = 0x34;
+    mem[0x00..0x06].copy_from_slice(&[0x21, 0x00, 0x21, 0x00, 0x00, 0x00]);
+    mem[0x100..0x106].copy_from_slice(&[0x80, 0x14, 0x80, 0x14, 0x00, 0xEE]);
 
     cpu.run();
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
 }
